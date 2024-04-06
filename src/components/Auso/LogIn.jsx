37e2308fd20ed28ba98e4b77a547c85e7d6eb3d4
khojiakbarr <chinozbekov09@gmail.com>
2024-04-06 14:39:45 +0500

    update

---
+++ src/components/Auso/LogIn.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+export default function LogIn() {
+  return (
+    <div className="container">
+      <div className="card">
+        <h1>Log In</h1>
+        <form>
+          <input type="text" />
+          <input type="text" />
+        </form>
+      </div>
+    </div>
+  );
+}
